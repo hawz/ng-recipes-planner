e46a3f7d9dfaa0e2081d93042c95cbffb4d3739b
Alberto Masia <alberto.masia@gmail.com>
2019-04-15 16:52:29 +0200

    Add loading subscription from uiService to login component.

--- src/app/auth/login/login.component.ts
+++ src/app/auth/login/login.component.ts
@@ -1,21 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { UIService } from 'src/app/shared/ui.service';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
-export class LoginComponent implements OnInit {
+export class LoginComponent implements OnInit, OnDestroy {
   isLoading = false;
+  private loadingSubscription: Subscription;
 
-  constructor() { }
+  constructor(private uiService: UIService) { }
 
   ngOnInit() {
+    this.loadingSubscription = this.uiService.loadingStateChanged.subscribe(
+      isLoading => { this.isLoading = isLoading; }
+    );
   }
 
   onSubmit(loginForm: NgForm) {
     console.log('submit', loginForm);
+  }
+
+  ngOnDestroy() {
+    if (this.loadingSubscription) {
+      this.loadingSubscription.unsubscribe();
+    }
   }
 
 }
