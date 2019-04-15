c7636b08108e383ca5526326c04b8f632e2ae4ce
Alberto Masia <alberto.masia@gmail.com>
2019-04-15 11:58:36 +0200

    Start using sidenav-container in app.component.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,7 +1,9 @@
 import { NgModule } from '@angular/core';
 
+import { MatSidenavModule } from '@angular/material/sidenav';
+
 @NgModule({
-  imports: [],
-  exports: []
+  imports: [MatSidenavModule],
+  exports: [MatSidenavModule]
 })
 export class MaterialModule { }
