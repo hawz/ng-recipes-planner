674f411d551caf01717d02b59c084c2c615d90e5
Alberto Masia <alberto.masia@gmail.com>
2019-04-15 15:23:54 +0200

    Create firebase app and add config object to env.

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyBA_ZhapdUwXBXguCo1MQcW_B0NXtv5-QA',
+    authDomain: 'ng-meal-planner-cdfa4.firebaseapp.com',
+    databaseURL: 'https://ng-meal-planner-cdfa4.firebaseio.com',
+    projectId: 'ng-meal-planner-cdfa4',
+    storageBucket: 'ng-meal-planner-cdfa4.appspot.com',
+    messagingSenderId: '593866636203'
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,15 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyBA_ZhapdUwXBXguCo1MQcW_B0NXtv5-QA',
+    authDomain: 'ng-meal-planner-cdfa4.firebaseapp.com',
+    databaseURL: 'https://ng-meal-planner-cdfa4.firebaseio.com',
+    projectId: 'ng-meal-planner-cdfa4',
+    storageBucket: 'ng-meal-planner-cdfa4.appspot.com',
+    messagingSenderId: '593866636203'
+  }
 };
 
 /*
