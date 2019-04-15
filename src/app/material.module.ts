b58995966011c8b0251728b0b924c2baa79e2cdc
Alberto Masia <alberto.masia@gmail.com>
2019-04-15 12:43:44 +0200

    Add links (routes yet to implement) to sidenav. Add EventEmitter in order to close sidenav from app.component.

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -4,9 +4,10 @@ import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
 
 @NgModule({
-  imports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule],
-  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule]
+  imports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule],
+  exports: [MatSidenavModule, MatToolbarModule, MatButtonModule, MatIconModule, MatListModule]
 })
 export class MaterialModule { }
--- src/app/navigation/sidenav-list/sidenav-list.component.ts
+++ src/app/navigation/sidenav-list/sidenav-list.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-sidenav-list',
@@ -6,10 +6,19 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav-list.component.css']
 })
 export class SidenavListComponent implements OnInit {
+  @Output() sidenavClose = new EventEmitter<void>();
 
   constructor() { }
 
   ngOnInit() {
   }
 
+  onClose() {
+    this.sidenavClose.emit();
+  }
+
+  onLogout() {
+    this.onClose();
+    console.log('logout user');
+  }
 }
