d7ccf19ef63cd045c9202c3a93971063dd436a76
Alberto Masia <alberto.masia@gmail.com>
2019-04-15 16:47:04 +0200

    Move shared stuff to separate shared module in order to include same modules once across the app.

--- src/app/auth/auth.module.ts
+++ src/app/auth/auth.module.ts
@@ -1,14 +1,17 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared/shared.module';
 
 import { AuthRoutingModule } from './auth-routing.module';
 import { SignupComponent } from './signup/signup.component';
 import { LoginComponent } from './login/login.component';
 
 @NgModule({
-  declarations: [SignupComponent, LoginComponent],
+  declarations: [
+    SignupComponent,
+    LoginComponent
+  ],
   imports: [
-    CommonModule,
+    SharedModule,
     AuthRoutingModule
   ],
   exports: []
