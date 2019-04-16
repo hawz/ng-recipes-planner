ef8d51019669929e5172bcf62e405f1f47188a96
Alberto Masia <alberto.masia@gmail.com>
2019-04-16 12:39:06 +0200

    Create structure for displaying next week's meals.

--- src/app/recipes/recipes-calendar/recipes-calendar.component.ts
+++ src/app/recipes/recipes-calendar/recipes-calendar.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
+import { DailyMenu } from '../dailyMenu.model';
 
 @Component({
   selector: 'app-recipes-calendar',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./recipes-calendar.component.css']
 })
 export class RecipesCalendarComponent implements OnInit {
+  today: Date = new Date();
+  weekMenu: DailyMenu[] = [];
 
   constructor() { }
 
   ngOnInit() {
+    // here we should search for recipes saved for the next week ahead
+    for (let index = 0; index < 7; index++) {
+      const nextDay = new Date();
+      nextDay.setDate(nextDay.getDate() + index);
+      const dailyMenu = {
+        date: nextDay,
+        day: moment(nextDay).format('dddd'),
+        lunch: [],
+        breakfast: [],
+        dinner: []
+      };
+      this.weekMenu.push(dailyMenu);
+    }
   }
 
 }
