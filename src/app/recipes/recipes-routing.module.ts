53df0f63c0c0cc941f2296993e4c8e6028c1ab83
Alberto Masia <alberto.masia@gmail.com>
2019-04-17 13:00:04 +0200

    Add route for single recipe to recipesRouting.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { RecipesComponent } from './recipes/recipes.component';
+import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 
 const routes: Routes = [
   { path: '', component: RecipesComponent },
+  { path: ':id', component: RecipeDetailsComponent }
 ];
 
 @NgModule({
