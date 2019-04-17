39d9db8074dd0560b42f2b1147b2c92163cedafa
Alberto Masia <alberto.masia@gmail.com>
2019-04-17 09:39:48 +0200

    Add shortenPipe to recipes module.

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,9 +5,15 @@ import { RecipesRoutingModule } from './recipes-routing.module';
 import { RecipesComponent } from './recipes/recipes.component';
 import { RecipesCalendarComponent } from './recipes-calendar/recipes-calendar.component';
 import { RecipesListComponent } from './recipes-list/recipes-list.component';
+import { ShortenPipe } from '../shared/shorten.pipe';
 
 @NgModule({
-  declarations: [RecipesComponent, RecipesCalendarComponent, RecipesListComponent],
+  declarations: [
+    RecipesComponent,
+    RecipesCalendarComponent,
+    RecipesListComponent,
+    ShortenPipe
+  ],
   imports: [
     SharedModule,
     RecipesRoutingModule
