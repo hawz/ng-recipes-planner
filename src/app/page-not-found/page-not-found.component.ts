54050becad5c16161dd03da3200f4aba90155827
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 12:24:24 +0200

    Add comments and clean WelcomeComponent.

--- src/app/page-not-found/page-not-found.component.ts
+++ src/app/page-not-found/page-not-found.component.ts
@@ -10,7 +10,7 @@
  *
  */
 
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-page-not-found',
--- src/app/welcome/welcome.component.ts
+++ src/app/welcome/welcome.component.ts
@@ -1,15 +1,19 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ *
+ *                WELCOME COMPONENT
+ *
+ * The WelcomeComponent is responsible for displaying the
+ * landing page template when a user is not authenticated.
+ *
+ * The '/' main route leads here.
+ *
+ */
+
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-welcome',
   templateUrl: './welcome.component.html',
   styleUrls: ['./welcome.component.css']
 })
-export class WelcomeComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class WelcomeComponent { }
