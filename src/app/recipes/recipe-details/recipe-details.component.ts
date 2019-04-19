ecc8e322340e9b7608abda38b41350ff9ee2486c
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 17:03:01 +0200

    Add comments to RecipeDetails component.

--- src/app/recipes/recipe-details/recipe-details.component.ts
+++ src/app/recipes/recipe-details/recipe-details.component.ts
@@ -1,3 +1,11 @@
+/**
+ * RECIPE-DETAILS COMPONENT
+ *
+ * The RecipeDetailsComponent is responsible for displaying the recipe
+ * details of a given recipe, specified by the recipe id in the URL.
+ *
+ *
+ */
 import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute, Params, Data } from '@angular/router';
 import { RecipesService } from '../recipes.service';
@@ -15,6 +23,7 @@ import { UIService } from 'src/app/shared/ui.service';
 })
 export class RecipeDetailsComponent implements OnInit, OnDestroy {
   id: number;
+  // empty recipe
   recipe: Recipe = {
     name: '',
     category: '',
@@ -27,6 +36,12 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
   isEdit = false;
   loadingSubs: Subscription;
 
+  /**
+   * @param route Contains info about the active route, such as data or queryParams
+   * @param recipesService Recipes service used for adding / removing a recipe
+   * @param dialog Service used to open a dialog modal
+   * @param uiService Service used to handle the loading spinner
+   */
   constructor(
     public route: ActivatedRoute,
     private recipesService: RecipesService,
@@ -34,9 +49,17 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
     private uiService: UIService
   ) { }
 
+  /**
+   * When initializing the component, the route.data observable is subscribed in order to receive
+   * the resolved recipe from the recipe resolver service. Also, the route.queryParams observable
+   * is subscribed in order to detect if we're just displaying a recipe or removing it.
+   * The loadingStateChanged subject from the uiService is also subscribed in order to show / hide
+   * the loading spinner.
+   */
   ngOnInit() {
     this.route.data.subscribe(
       (data: Data) => {
+        // the recipe is assigned to the one resolved by the recipe resolver
         this.recipe = data['recipe'];
       }
     );
@@ -52,6 +75,12 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
     );
   }
 
+  /**
+   * onAddRecipeToMenu is responsible for opening the dialog modal where the user
+   * picks the date and meal to save the selected recipe. If the modal is closed with
+   * a truthy result, then this method calls the addRecipe method from the recipesService
+   * which is responsible for saving the recipe to the DB.
+   */
   onAddRecipeToMenu() {
     const dialogRef = this.dialog.open(RecipeAddComponent, {
       data: {
@@ -66,6 +95,12 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * onRemoveRecipeFromMenu is responsible for opening a confirmation modal
+   * and (if the deletion is confirmed by the user) for calling the
+   * removeRecipe method from the recipesService, passing the selected recipe,
+   * but also the meal and seconds value fetched by the route.queryParams.
+   */
   onRemoveRecipeFromMenu() {
     const dialogRef = this.dialog.open(RecipeRemoveComponent, {
       data: {
@@ -82,6 +117,10 @@ export class RecipeDetailsComponent implements OnInit, OnDestroy {
     });
   }
 
+  /**
+   * When destroying the component instance, this lifecycle hooks is meant
+   * to unsubscribe the loadingSubs subscription, thus avoiding performance issues.
+   */
   ngOnDestroy() {
     if (this.loadingSubs) {
       this.loadingSubs.unsubscribe();
