26a420a8cffef6ef7cb8dfa15cc3573c96e17d97
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 16:34:53 +0200

    Add comments to RecipesRoutingModule.

--- src/app/recipes/recipes-routing.module.ts
+++ src/app/recipes/recipes-routing.module.ts
@@ -1,3 +1,14 @@
+/**
+ *
+ *                      RECIPES-ROUTING MODULE
+ *
+ * The RecipesRoutingModule defines the three lazy loaded routes handled inside the RecipesModule.
+ *
+ * For the single recipe routes (both handled by the RecipeDetails component) it gets
+ * the resolved recipe (from the GET /recipes/:id API call) from the RecipeResolver
+ * service.
+ */
+
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
@@ -5,6 +16,12 @@ import { RecipesComponent } from './recipes/recipes.component';
 import { RecipeDetailsComponent } from './recipe-details/recipe-details.component';
 import { RecipeResolver } from './recipe-resolver.service';
 
+/**
+ * The routes here defined are:
+ * - recipes --> handled by the RecipesComponent
+ * - recipes/:id --> handled by the RecipeDetailsComponent
+ * - recipes/:id/edit --> handled by the RecipeDetailsComponent as well
+ */
 const routes: Routes = [
   { path: '', component: RecipesComponent },
   { path: ':id', component: RecipeDetailsComponent, resolve: { recipe: RecipeResolver } },
