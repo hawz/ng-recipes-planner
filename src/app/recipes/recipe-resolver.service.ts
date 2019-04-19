67a5d2db6a893275ba5880c47e4a2f1aac0a6343
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 12:08:05 +0200

    Add comments to RecipeResolver class.

--- src/app/recipes/recipe-resolver.service.ts
+++ src/app/recipes/recipe-resolver.service.ts
@@ -1,3 +1,14 @@
+/**
+ *                  RECIPE RESOLVER
+ *
+ * The RecipeResolver class acts as a data provider
+ * implementing the Resolve interface and providing
+ * a Recipe whenever a single recipe route is fetched:
+ *
+ * - /recipes/:id --> Recipe details
+ * - /recipes/:id --> Recipe edit (just to remove a recipe from the calendar)
+ */
+
 import { Injectable } from '@angular/core';
 import { Resolve, ActivatedRouteSnapshot } from '@angular/router';
 import { Recipe } from './recipe.model';
@@ -8,8 +19,14 @@ import { RecipesService } from './recipes.service';
   providedIn: 'root'
 })
 export class RecipeResolver implements Resolve<Recipe> {
+  /**
+   * @param recipesService the recipes Service in order to call the fetchRecipe method passing a specific recipeId
+   */
   constructor(private recipesService: RecipesService) { }
 
+  /**
+   * @param route the activated route snapshot, used to fetch the id of the desired recipe.
+   */
   resolve(route: ActivatedRouteSnapshot): Observable<Recipe> | Promise<Recipe> | Recipe {
     return this.recipesService.fetchRecipe(+route.params['id']);
   }
