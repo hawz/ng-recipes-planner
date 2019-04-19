e2fb1ffc450338335aaae4722818f4d5dcd26fe6
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 17:16:15 +0200

    Add comments to RecipesComponent and clean some code.

--- src/app/recipes/recipes/recipes.component.ts
+++ src/app/recipes/recipes/recipes.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+/**
+ * RECIPES COMPONENT
+ *
+ * The RecipesComponent acts as a container for the RecipesCalendar and RecipesList
+ * components. It displays a tab group with two tabs, each one containing a component.
+ *
+ */
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-recipes',
   templateUrl: './recipes.component.html',
   styleUrls: ['./recipes.component.css']
 })
-export class RecipesComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit() {
-  }
-
-}
+export class RecipesComponent { }
