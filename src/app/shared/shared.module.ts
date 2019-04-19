a0a76f52cfe1cef1029e094250d81f9491ffecd7
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 12:10:00 +0200

    Add comments to SharedModule.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,3 +1,11 @@
+/**
+ *
+ *                            SHARED MODULE
+ *
+ * The SharedModule is basically used to import and export modules which are
+ * shared by other modules, thus avoiding multiple imports of the same stuff inside those modules.
+ *
+ */
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
