241eecce744bc6252b6cd161f14e1f0017ff9457
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 12:41:52 +0200

    Add comments to dailyMenu and recipe models.

--- src/app/recipes/dailyMenu.model.ts
+++ src/app/recipes/dailyMenu.model.ts
@@ -1,3 +1,21 @@
+/**
+ *
+ *            DAILY MENU MODEL
+ *
+ * The DailyMenu model is used as a container for recipes within the
+ * week inside the recipesCalendar component. Basically a week is defined
+ * as an array of this kind of objects: DailyMenu[].
+ *
+ * Each DailyMenu contains:
+ *
+ * - the day it refers to
+ * - the date it refers to
+ * - an array of recipes for breakfast
+ * - an array of recipes for lunch
+ * - an array of recipes for dinner
+ *
+ */
+
 import { Recipe } from './recipe.model';
 
 export interface DailyMenu {
