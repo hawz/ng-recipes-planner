74712122e868e64d52eb6541e4b34b9663286bff
Alberto Masia <alberto.masia@gmail.com>
2019-04-19 10:12:10 +0200

    Fix RecipeRemove component standard tests.

--- src/app/recipes/recipe-remove/recipe-remove.component.spec.ts
+++ src/app/recipes/recipe-remove/recipe-remove.component.spec.ts
@@ -1,6 +1,9 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { RecipeRemoveComponent } from './recipe-remove.component';
+import { MaterialModule } from 'src/app/material.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material';
 
 describe('RecipeRemoveComponent', () => {
   let component: RecipeRemoveComponent;
@@ -8,7 +11,15 @@ describe('RecipeRemoveComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ RecipeRemoveComponent ]
+      declarations: [ RecipeRemoveComponent ],
+      imports: [
+        BrowserAnimationsModule,
+        MaterialModule
+      ],
+      providers: [
+        { provide: MAT_DIALOG_DATA, useValue: { recipe: { name: 'Test Recipe' } } },
+        { provide: MatDialogRef, useValue: {} }
+      ]
     })
     .compileComponents();
   }));
